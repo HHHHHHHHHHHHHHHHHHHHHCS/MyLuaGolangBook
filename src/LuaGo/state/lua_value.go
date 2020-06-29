4d2f1d4f44a2f9db5815f2aa7a89859ad501bf61
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2020-06-29 20:16:05 +0800

    add

---
+++ src/LuaGo/state/lua_value.go
@@ -0,0 +1,25 @@
+package state
+
+import (
+	. "LuaGo/api"
+)
+
+type luaValue interface{}
+
+func typeof(val luaValue) LuaType {
+	//x.(type) 只能在switch 中使用
+	switch val.(type) {
+	case nil:
+		return LUA_TNIL
+	case bool:
+		return LUA_TBOOLEAN
+	case int64:
+		return LUA_TNUMBER
+	case float64:
+		return LUA_TNUMBER
+	case string:
+		return LUA_TSTRING
+	default:
+		panic("todo!")
+	}
+}

5851ed84ef24db5b8a7302b2249292d1f4119c48
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2020-08-03 20:29:34 +0800

    add

--- src/LuaGo/state/lua_state.go
+++ src/LuaGo/state/lua_state.go
@@ -1,11 +1,18 @@
 package state
 
+import "LuaGo/binchunk"
+
 type luaState struct {
 	stack *luaStack
+	proto *binchunk.Prototype
+	pc    int
 }
 
-func New() *luaState {
+//Lua栈初始容量 和 函数原型以初始化proto字段
+func New(stackSize int, proto *binchunk.Prototype) *luaState {
 	return &luaState{
-		stack: newLuaStack(20),
+		stack: newLuaStack(stackSize),
+		proto: proto,
+		pc:    0,
 	}
 }

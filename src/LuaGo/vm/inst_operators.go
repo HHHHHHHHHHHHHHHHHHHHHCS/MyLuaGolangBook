48dc68c7f5202967099b24fc6d6361084403fc31
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2020-08-13 20:28:05 +0800

    add

--- src/LuaGo/vm/inst_operators.go
+++ src/LuaGo/vm/inst_operators.go
@@ -95,3 +95,95 @@ func unm(i Instruction, vm LuaVM) {
 func bnot(i Instruction, vm LuaVM) {
 	_unaryArith(i, vm, LUA_OPBNOT)
 }
+
+//得到某个栈的值的长度(如string) 在塞入某个位置
+func _len(i Instruction, vm LuaVM) {
+	a, b, _ := i.ABC()
+	a += 1
+	b += 1
+
+	vm.Len(b)
+	vm.Replace(a)
+}
+
+//for 循环字符串拼接  把结果替换到 某个位置
+//字符串拼接需要检测长度  不然容易栈溢出
+func concat(i Instruction, vm LuaVM) {
+	a, b, c := i.ABC()
+	a += 1
+	b += 1
+	c += 1
+
+	n := c - b + 1
+	vm.CheckStack(n)
+	for i := b; i <= c; i++ {
+		vm.PushValue(i)
+	}
+
+	vm.Concat(n)
+	vm.Replace(a)
+}
+
+//比较 如果失败 则跳过下一条指令
+func _compare(i Instruction, vm LuaVM, op CompareOp) {
+	a, b, c := i.ABC()
+
+	vm.GetRK(b)
+	vm.GetRK(c)
+	//a!=0 true   result!=true -> false ->jump 1 pc
+	if vm.Compare(-2, -1, op) != (a != 0) {
+		vm.AddPC(1)
+	}
+	vm.Pop(2)
+}
+
+//==
+func eq(i Instruction, vm LuaVM) {
+	_compare(i, vm, LUA_OPEQ)
+}
+
+//<
+func lt(i Instruction, vm LuaVM) {
+	_compare(i, vm, LUA_OPLT)
+}
+
+//<=
+func le(i Instruction, vm LuaVM) {
+	_compare(i, vm, LUA_OPLE)
+}
+
+//读取栈索引b bool 取反 插入 栈索引a
+func not(i Instruction, vm LuaVM) {
+	a, b, _ := i.ABC()
+	a += 1
+	b += 1
+
+	vm.PushBoolean(!vm.ToBoolean(b))
+	vm.Replace(a)
+}
+
+//索引b的bool 值 是否和c 一样
+//如果一样 则把 b 的值 赋值到栈索引a
+//否则跳过下一条指令
+func testSet(i Instruction, vm LuaVM) {
+	a, b, c := i.ABC()
+	a += 1
+	b += 1
+
+	if vm.ToBoolean(b) == (c != 0) {
+		vm.Copy(b, a)
+	} else {
+		vm.AddPC(1)
+	}
+}
+
+//读取栈索引a  和 c 布尔比较
+//如果失败 则跳过下一条指令
+func test(i Instruction, vm LuaVM) {
+	a, _, c := i.ABC()
+	a += 1
+
+	if vm.ToBoolean(a) != (c != 0) {
+		vm.AddPC(1)
+	}
+}

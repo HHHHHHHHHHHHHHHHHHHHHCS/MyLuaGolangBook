7f503ac7f5baea7c9b3f52bc54ebb2b4ae2d23de
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2020-08-25 20:24:49 +0800

    add

--- src/LuaGo/state/api_get.go
+++ src/LuaGo/state/api_get.go
@@ -1,6 +1,9 @@
 package state
 
-import . "LuaGo/api"
+import (
+	. "LuaGo/api"
+	. "LuaGo/vm"
+)
 
 func (self *luaState) CreateTable(nArr, nRec int) {
 	t := newLuaTable(nArr, nRec)
@@ -26,4 +29,55 @@ func (self *luaState) getTable(t, k luaValue) LuaType {
 		return typeOf(v)
 	}
 	panic("not a table!")
+}
+
+//查找表  查找key得到value  通过 idx 得到表    k 是key 可能会自动转换为int 去查找
+func (self *luaState) GetField(idx int, k string) LuaType {
+	t := self.stack.get(idx)
+	return self.getTable(t, k)
+}
+
+//查找表  但是这时候key 是int 效率高点 不用判断和转换
+func (self *luaState) GetI(idx int, i int64) LuaType {
+	t := self.stack.get(idx)
+	return self.getTable(t, i)
+}
+
+//查找idx 的表  栈顶弹出两个值当作 k,v  存入表
+func (self *luaState) SetTable(idx int) {
+	t := self.stack.get(idx)
+	v := self.stack.pop()
+	k := self.stack.pop()
+	self.setTable(t, k, v)
+}
+
+//存入表数据
+func (self *luaState) setTable(t, k, v luaValue) {
+	if tbl, ok := t.(*luaTable); ok {
+		tbl.put(k, v)
+		return
+	}
+	panic("not a table")
+}
+
+//存入表key位置(string) 数据
+func (self *luaState) SetField(idx int, k string) {
+	t := self.stack.get(idx)
+	v := self.stack.pop()
+	self.setTable(t, k, v)
+}
+
+//存入表key位置(int) 数据
+func (self *luaState) SetI(idx int, i int64) {
+	t := self.stack.get(idx)
+	v := self.stack.pop()
+	self.setTable(t, i, v)
+}
+
+func newTable(i Instruction,vm LuaVM){
+	a,b,c :=i.ABC()
+	a+=1
+
+	vm.CreateTable(Fb2int(b),Fb2int(c))
+	vm.Replace(a)
 }

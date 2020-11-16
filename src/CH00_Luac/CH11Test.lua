a21c41c4b975d69cf083809a21d0e1cfe750af3f
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2020-11-16 20:43:21 +0800

    add

--- src/CH00_Luac/CH11Test.lua
+++ src/CH00_Luac/CH11Test.lua
@@ -1,27 +1,4 @@
-print(getmetatable("foo"))
-print(getmetatable("bar"))
-print(getmetatable(nil))
-print(getmetatable(false))
-print(getmetatable(100))
-print(getmetatable({  }))
-print(getmetatable(print))
-
-print("====================================")
-
-t = {}
-mt = {}
-setmetatable(t, mt)
-print(getmetatable(t) == mt)
---对于非表类型的 可以用debug.setmetatable
-debug.setmetatable(100, mt)
-print(getmetatable(200) == mt)
-
-print("====================================")
-
-mt = {}
-mt.__add = function(v1, v2)
-    return vector(v1.x + v2.x, v1.y + v2.y)
-end
+local mt = {}
 
 function vector(x, y)
     local v = { x = x, y = y }
@@ -29,7 +6,51 @@ function vector(x, y)
     return v
 end
 
+mt.__add = function(v1, v2)
+    return vector(v1.x + v2.x, v1.y + v2.y)
+end
+
+mt.__sub = function(v1, v2)
+    return vector(v1.x - v2.x, v1.y - v2.y)
+end
+
+mt.__mul = function(v1, n)
+    return vector(v1.x * n, v1.y * n)
+end
+
+mt.__div = function(v1, n)
+    return vector(v1.x / n, v1.y / n)
+end
+
+mt.__len = function(v)
+    return (v.x * v.x + v.y * v.y) ^ 0.5
+end
+
+mt.__eq = function(v1, v2)
+    return v1.x == v2.x and v1.y == v2.y
+end
+
+mt.__index = function(v, k)
+    if k == "print" then
+        return function()
+            print("[" .. v.x .. " , " .. v.y .. "]")
+        end
+    end
+end
+
+mt.__call = function(v)
+    print("[" .. v.x .. " , " .. v.y .. "]")
+end
+
 v1 = vector(1, 2)
-v2 = vector(3, 5)
-v3 = v1 + v2
-print(v3.x, v3.y)
+v1:print()
+v2 = vector(3, 4)
+v2:print()
+v3 = v1 * 2
+v3.print()
+v4 = v1 + v3
+v4:print()
+print(#v2)
+print(v1 == v2)
+print(v2 == vector(3, 4))
+v4()

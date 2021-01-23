ee0973a4945533c07272f58b8b49de3f4b06082c
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2021-01-23 12:25:13 +0800

    add

--- src/LuaGo/compiler/lexer.go
+++ src/LuaGo/compiler/lexer.go
@@ -1,1 +1,242 @@
-package compiler
+package lexer
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+var reOpeningLongBracket = regexp.MustCompile(`^\[=*\[`)
+var reNewLine = regexp.MustCompile(`\r\n|\n\r|\n|\r`)
+
+type Lexer struct {
+	chunk     string //源代码
+	chunkName string //源文件名
+	line      int    //当前行号
+}
+
+//创建行号为1的Lexer
+func NewLexer(chunk, chunkName string) *Lexer {
+	return &Lexer{chunk, chunkName, 1}
+}
+
+//判断字符串的开头
+func (self *Lexer) test(s string) bool {
+	return strings.HasPrefix(self.chunk, s)
+}
+
+//跳过n个字符
+func (self *Lexer) next(n int) {
+	self.chunk = self.chunk[n:]
+}
+
+//判断是否是空白字符
+func isWhiteSpace(c byte) bool {
+	switch c {
+	case '\t', '\n', '\v', '\f', '\r', ' ':
+		return true
+	}
+	return false
+}
+
+//是回车或者换行
+func isNewLine(c byte) bool {
+	return c == '\r' || c == '\n'
+}
+
+func (self *Lexer) error(f string, a ...interface{}) {
+	err := fmt.Sprintf(f, a...)
+	err = fmt.Sprintf("%s:%d: %s", self.chunkName, self.line, err)
+	panic(err)
+}
+
+//去除长注释
+func (self *Lexer) scanLongString() string {
+	//找出[=?[长注释的头
+	openingLongBracket := reOpeningLongBracket.FindString(self.chunk)
+	if openingLongBracket == "" {
+		self.error("invalid long string delimiter near '%s'", self.chunk[0:2])
+	}
+
+	//头尾数量应该对齐 所以替换
+	closingLongBracket := strings.Replace(openingLongBracket, "[", "]", -1)
+	//找出尾
+	closingLongBracketIdx := strings.Index(self.chunk, closingLongBracket)
+	if closingLongBracketIdx < 0 {
+		self.error("unfinished long string or comment")
+	}
+
+	str := self.chunk[len(openingLongBracket):closingLongBracketIdx]
+	//跳过尾开始index + 尾长度
+	self.next(closingLongBracketIdx + len(closingLongBracket))
+
+	str = reNewLine.ReplaceAllString(str, "\n")
+	self.line += strings.Count(str, "\n")
+	//跳过第一个换行符
+	if len(str) > 0 && str[0] == '\n' {
+		str = str[1:]
+	}
+	return str
+}
+
+//跳过注释
+func (self *Lexer) skipComment() {
+	self.next(2)        // skip --
+	if self.test("[") { //long comment ?
+		if reOpeningLongBracket.FindString(self.chunk) != "" {
+			self.scanLongString()
+			return
+		}
+	}
+	for len(self.chunk) > 0 && !isNewLine(self.chunk[0]) {
+		self.next(1)
+	}
+}
+
+//跳过一些换行和注释
+func (self *Lexer) skipWhiteSpaces() {
+	for len(self.chunk) > 0 {
+		if self.test("--") {
+			self.skipComment()
+		} else if self.test("\r\n") || self.test("\n\r") {
+			self.next(2)
+			self.line += 1
+		} else if isNewLine(self.chunk[0]) {
+			self.next(1)
+			self.line += 1
+		} else if isWhiteSpace(self.chunk[0]) {
+			self.next(1)
+		} else {
+			break
+		}
+
+	}
+}
+
+func (self *Lexer) NextToken() (line, kind int, token string) {
+	self.skipWhiteSpaces()
+	if len(self.chunk) == 0 {
+		return self.line, TOKEN_EOF, "EOF"
+	}
+	switch self.chunk[0] {
+	case ';':
+		self.next(1)
+		return self.line, TOKEN_SEP_SEMI, ";"
+	case ',':
+		self.next(1)
+		return self.line, TOKEN_SEP_COMMA, ","
+	case '(':
+		self.next(1)
+		return self.line, TOKEN_SEP_LPAREN, "("
+	case ')':
+		self.next(1)
+		return self.line, TOKEN_SEP_RPAREN, ")"
+	case ']':
+		self.next(1)
+		return self.line, TOKEN_SEP_RBRACK, "]"
+	case '{':
+		self.next(1)
+		return self.line, TOKEN_SEP_LCURLY, "{"
+	case '}':
+		self.next(1)
+		return self.line, TOKEN_SEP_RCURLY, "}"
+	case '+':
+		self.next(1)
+		return self.line, TOKEN_OP_ADD, "+"
+	case '-':
+		self.next(1)
+		return self.line, TOKEN_OP_MINUS, "-"
+	case '*':
+		self.next(1)
+		return self.line, TOKEN_OP_MUL, "*"
+	case '^':
+		self.next(1)
+		return self.line, TOKEN_OP_POW, "^"
+	case '%':
+		self.next(1)
+		return self.line, TOKEN_OP_MOD, "%"
+	case '&':
+		self.next(1)
+		return self.line, TOKEN_OP_BAND, "&"
+	case '|':
+		self.next(1)
+		return self.line, TOKEN_OP_BOR, "|"
+	case '#':
+		self.next(1)
+		return self.line, TOKEN_OP_LEN, "#"
+	case ':':
+		if self.test("::") {
+			self.next(2)
+			return self.line, TOKEN_SEP_LABEL, "::"
+		} else {
+			self.next(1)
+			return self.line, TOKEN_SEP_COLON, ":"
+		}
+	case '/':
+		if self.test("//") {
+			self.next(2)
+			return self.line, TOKEN_OP_IDIV, "//"
+		} else {
+			self.next(1)
+			return self.line, TOKEN_OP_DIV, "/"
+		}
+	case '~':
+		if self.test("~=") {
+			self.next(2)
+			return self.line, TOKEN_OP_NE, "~="
+		} else {
+			self.next(1)
+			return self.line, TOKEN_OP_WAVE, "~"
+		}
+	case '=':
+		if self.test("==") {
+			self.next(2)
+			return self.line, TOKEN_OP_EQ, "=="
+		} else {
+			self.next(1)
+			return self.line, TOKEN_OP_ASSIGN, "="
+		}
+	case '<':
+		if self.test("<<") {
+			self.next(2)
+			return self.line, TOKEN_OP_SHL, "<<"
+		} else if self.test("<=") {
+			self.next(2)
+			return self.line, TOKEN_OP_LE, "<="
+		} else {
+			self.next(1)
+			return self.line, TOKEN_OP_LT, "<"
+		}
+	case '>':
+		if self.test(">>") {
+			self.next(2)
+			return self.line, TOKEN_OP_SHR, ">>"
+		} else if self.test(">=") {
+			self.next(2)
+			return self.line, TOKEN_OP_GE, ">="
+		} else {
+			self.next(1)
+			return self.line, TOKEN_OP_GT, ">"
+		}
+	case '.':
+		if self.test("...") {
+			self.next(3)
+			return self.line, TOKEN_VARARG, "..."
+		} else if self.test("..") {
+			self.next(2)
+			return self.line, TOKEN_OP_CONCAT, ".."
+		} else if len(self.chunk) == 1 || !isDigit(self.chunk[1]) {
+			self.next(1)
+			return self.line, TOKEN_SEP_DOT, "."
+		}
+	case '[':
+		if self.test("[[") || self.test("[=") {
+			return self.line, TOKEN_STRING, self.scanLongString()
+		} else {
+			self.next(1)
+			return self.line, TOKEN_SEP_LBRACK, "["
+		}
+	case '\'', '"':
+		return self.line, TOKEN_STRING, self.scanShortString()
+	}
+}

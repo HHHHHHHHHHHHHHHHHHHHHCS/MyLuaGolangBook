564dbfd4d48214a061df4cde66b98540af3d9d74
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2021-02-06 15:23:46 +0800

    add

---
+++ src/LuaGo/compiler/ast/exp.go
@@ -0,0 +1,92 @@
+package ast
+
+type Exp interface{}
+
+//简单的赋值表达式
+//---------------------
+type NilExp struct{ Line int }
+type TrueExp struct{ Line int }
+type FalseExp struct{ Line int }
+type VarargExp struct{ Line int }
+type IntegerExp struct {
+	Line int
+	Val  int64
+}
+type FloatExp struct {
+	Line int
+	Val  float64
+}
+type StringExp struct {
+	Line int
+	Str  string
+}
+type NameExp struct {
+	Line int
+	Name string
+}
+
+//运算符表达式
+//------------
+
+//一元运算符
+type UnopExp struct {
+	Line int
+	Op   int
+	Exp  Exp
+}
+
+//二元运算符
+type BinopExp struct {
+	Line int
+	Op   int
+	Exp1 Exp
+	Exp2 Exp
+}
+
+//拼接表达式
+type ConcatExp struct {
+	Line int
+	Exps []Exp
+}
+
+//方法表达式
+type FuncDefExp struct {
+	Line     int
+	LastLine int
+	ParList  []string
+	IsVararg bool
+	Block    *Block
+}
+
+//圆括号表达式
+type ParensExp struct {
+	Exp Exp
+}
+
+//表访问表达式
+type TableAccessExp struct {
+	LastLine  int //line of ']'
+	PrefixExp Exp
+	KeyExp    Exp
+}
+
+//使用方法表达式
+type FuncCallExp struct {
+	Line      int // line of '('
+	LastLine  int // line of ')'
+	PrefixExp Exp
+	NameExp   *StringExp
+	Args      []Exp
+}
+
+//表的构造表达式
+//tableconstructor ::= '{' [fieldlist] '}'
+//fieldlist ::= field {fieldsep field}[fieldsep]
+//field ::= '[' exp ']' '=' exp | Name '=' exp | exp
+//fieldsep ::= ',' | ';'
+type TableConstructorExp struct {
+	Line     int //line of '{'
+	LastLine int //line of '}'
+	KeyExps  []Exp
+	ValExps  []Exp
+}
--- src/LuaGo/compiler/ast/stat.go
+++ src/LuaGo/compiler/ast/stat.go
@@ -1,5 +1,22 @@
 package ast
 
+/*
+stat ::=  ‘;’ |
+	 varlist ‘=’ explist |
+	 functioncall |
+	 label |
+	 break |
+	 goto Name |
+	 do block end |
+	 while exp do block end |
+	 repeat block until exp |
+	 if exp then block {elseif exp then block} [else block] end |
+	 for Name ‘=’ exp ‘,’ exp [‘,’ exp] do block end |
+	 for namelist in explist do block end |
+	 function funcname funcbody |
+	 local function Name funcbody |
+	 local namelist [‘=’ explist]
+*/
 type Stat interface {
 }
 
@@ -35,18 +52,21 @@ type DoStat struct {
 //调用函数 可以是语句也可以是表达式
 type FuncCallStat = FuncCallExp
 
+//while循环
 //while exp do block end
-type whileStat struct {
+type WhileStat struct {
 	Exp   Exp
 	Block *Block
 }
 
+//repeat循环
 //repeat block until exp
 type RepeatStat struct {
 	Block *Block
 	Exp   Exp
 }
 
+//if语句
 //if exp then block {elseif exp then block} [else block] end
 //如果把最后的else 看做 elseif true 则可以缩写为
 //if exp then block {elseif exp then block} [elseif true then block] end
@@ -54,20 +74,25 @@ type RepeatStat struct {
 //索引0是 if then 其余都是 elseif
 type IfStat struct {
 	Exps   []Exp
-	Blocks []*Blocks
+	Blocks []*Block
 }
 
+//for循环
 //for Name '=' exp ',' [',' exp] do block end
 type ForNumStat struct {
 	LineOfFor int
 	LineOfDo  int
 	VarName   string
 	InitExp   Exp
+	LimitExp  Exp
 	StepExp   Exp
 	Block     *Block
 }
 
+//for in 循环
 //for nameList in explist do block end
+// namelist ::= Name {‘,’ Name}
+// explist ::= exp {‘,’ exp}
 type ForInStat struct {
 	LineOfDo int
 	NameList []string
@@ -75,6 +100,8 @@ type ForInStat struct {
 	Block    *Block
 }
 
+//local变量申明
+//要把末尾行号记录下来 以供生成阶段使用
 //local namelist ['=' explist]
 //namelist ::= Name {',' Name}
 //explist ::= exp{',' exp}
@@ -82,4 +109,27 @@ type LocalVarDeclStat struct {
 	LastLine int
 	NameList []string
 	ExpList  []Exp
+}
+
+//赋值语句
+//varlist '=' explist
+//varlist ::= var {',' var}
+//var :: Name | prefixexp '[' exp ']' | prefixexp '.' Name
+//explist ::= exp {',' exp}
+type AssignStat struct {
+	LastLine int
+	VarList  []Exp
+	ExpList  []Exp
+}
+
+//非局部函数
+//function funcName funcBody
+//	=> function t.a.b.c:f(param) body end
+//funcName ::= Name {'.' Name} [':' Name]
+//	=> function t.a.b.c.f(self,param) body end
+//funcBody ::= '(' [parlist] ')' block end
+//	=> t.a.b.c.f = function(self, params) body end
+type LocalFuncDefStat struct {
+	Name string
+	Exp  *FuncDefExp
 }

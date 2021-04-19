6f20d4b3738adcc64acf3dae80d466b55d660fb1
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2021-04-19 21:47:24 +0800

    add

--- src/LuaGo/compiler/codegen/cg_exp.go
+++ src/LuaGo/compiler/codegen/cg_exp.go
@@ -1,6 +1,9 @@
 package codegen
 
-import . "LuaGo/compiler/ast"
+import (
+	. "LuaGo/compiler/ast"
+	. "LuaGo/vm"
+)
 
 func cgExp(fi *funcInfo, node Exp, a, n int) {
 	switch exp := node.(type) {
@@ -76,4 +79,62 @@ func cgFuncDefExp(fi *funcInfo, node *FuncDefExp, a int) {
 
 	bx := len(fi.subFuncs) - 1
 	fi.emitClosure(a, bx) //退出
+}
+
+func (self *funcInfo) emitNewTable(a, nArr, nRec int) {
+	self.emitABC(OP_NEWTABLE, a, Int2fb(nArr), Int2fb(nRec))
+}
+
+//表构造表达式
+func cgTableConstructorExp(fi *funcInfo, node *TableConstructorExp, a int) {
+	nArr := 0
+	for _, keyExp := range node.KeyExps {
+		if keyExp == nil {
+			nArr++
+		}
+	}
+	nExps := len(node.KeyExps)
+	multRet := nExps > 0 && isVarargOrFuncCall(node.ValExps[nExps-1])
+
+	fi.emitNewTable(a, nArr, nExps-nArr)
+
+	arrIdx := 0
+
+	for i, keyExp := range node.KeyExps {
+		valExp := node.ValExps[i]
+
+		if keyExp == nil { //如果没有key则for 循环增加数组
+			arrIdx++
+			tmp := fi.allocReg()
+			if i == nExps-1 && multRet {
+				cgExp(fi, valExp, tmp, -1)
+			} else {
+				cgExp(fi, valExp, tmp, 1)
+			}
+
+			if arrIdx%50 == 0 || arrIdx == nArr { //LFIELDS_PER_FLUSH
+				n := arrIdx % 50
+				if n == 0 {
+					n = 50
+				}
+				c := (arrIdx-1)/50 + 1
+				fi.freeRegs(n)
+				if i == nExps-1 && multRet {
+					fi.emitSetList(a, 0, c)
+				} else {
+					fi.emitSetList(a, n, c)
+				}
+			}
+
+			continue
+		}
+
+		//关联表
+		b := fi.allocReg()
+		cgExp(fi, keyExp, b, 1)
+		c := fi.allocReg()
+		cgExp(fi, valExp, c, 1)
+		fi.freeRegs(2)
+		fi.emitSetTable(a, b, c)
+	}
 }
